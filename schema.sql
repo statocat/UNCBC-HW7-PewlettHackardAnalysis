cfe87aa56459f64d29e694add610b365e9955bba
statocat <catherineesmith1415@gmail.com>
2022-10-11 23:31:12 -0400

    update schema, add retirement_info table to db, add retirement_info.csv

--- schema.sql
+++ schema.sql
@@ -35,4 +35,33 @@ CREATE TABLE salaries(
 	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+DROP TABLE dept_emp
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no,from_date)
+);
+
+--Retirement Eligibility
+-- employees born in 1952-1955 with hired in 1985-1988
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- export retirement_info table to csv
