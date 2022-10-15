0a98ff5f9e17c6c62b0f22f77ebe6c4178cebfa8
statocat <catherineesmith1415@gmail.com>
2022-10-15 17:11:34 -0400

    update schema, add current_emp table and ccsv and dept_retirement table and csv

--- schema.sql
+++ schema.sql
@@ -61,7 +61,63 @@ CREATE TABLE titles(
 SELECT first_name, last_name
 INTO retirement_info
 FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
--- export retirement_info table to csv
+--see retirement_info table
+SELECT * FROM retirement_info;
+
+-- inner join depts and dept_manager table
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- join retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- join retirement_info and dept_emp tables using aliases
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+--Employee count by dept_no
+SELECT count(ce.emp_no), de.dept_no
+INTO dept_retirement_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--peek at the salaries table
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+--note: the dates don't seem to line up with most current dates of employment
+-- fix that by pulling employment dates from dept_emp table
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary,de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
