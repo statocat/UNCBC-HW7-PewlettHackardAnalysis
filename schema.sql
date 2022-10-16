43fae423391d8fb1107719246b30c888815e7a56
statocat <catherineesmith1415@gmail.com>
2022-10-16 12:11:28 -0400

    update schema

--- schema.sql
+++ schema.sql
@@ -121,3 +121,55 @@ ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+--peek at manager_info table
+--why do only 5 out of 9 depts have managers??
+SELECT * FROM manager_info;
+
+-- add depts to current employees table
+-- some people occuring twice
+SELECT ce.emp_no, ce.first_name, ce.last_name, d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- skill drill 
+-- retirement eligible employee numbers, first_name, last_name, employee dept == SALES
+SELECT ri.emp_no, ri.first_name,ri.last_name, d.dept_name 
+--INTO sales_info
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+LEFT JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales');
+
+--skill drill
+-- retirement eligible employee numbers, first_name, last_name, employee dept in (Sales, Development)
+SELECT ri.emp_no, ri.first_name,ri.last_name, d.dept_name 
+--INTO mentors_info
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+LEFT JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN('Sales','Development');
+
